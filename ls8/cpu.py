eefb6344b5c63c2b73adbcffbf10b1033792d4ec
Francis Bulus <notchera@gmail.com>
2020-03-24 09:58:00 +0100

    added ldr and prn variables

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,20 +2,23 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+
 class CPU:
     """Main CPU class."""
-
-    def ram_read(self, address):
-        return self.ram[address]
-
-    def ram_write(self, address, value):
-        self.ram[address] = value
 
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256    # 256 bytes of memory
         self.reg = [0] * 8      # 8 general-purpose registers
-        self.pc = 0 
+        self.pc = 0
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
 
     def load(self):
         """Load a program into memory."""
@@ -26,25 +29,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -56,8 +58,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -70,4 +72,16 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while True:
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            operand_count = ir >> 6
+            instruction_length = operand_count + 1
+
+            set_pc = ir >> 4 & 0b0001
+
+            ##
+
+            if not set_pc:
+                self.pc += instruction_length
